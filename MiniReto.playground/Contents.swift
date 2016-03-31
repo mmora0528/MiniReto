8bbc9b1be10ff24c3aeb2efa65f164562887fafd
mmora0528 <m4nu31mor@gmail.com>
2016-03-30 18:12:43 -0600

    Commit Minaret terminado

---
+++ MiniReto.playground/Contents.swift
@@ -0,0 +1,24 @@
+//Mini reto semana 2
+
+import UIKit
+
+var rango = 0...100
+
+for  i in rango{
+    if (i % 5) == 0{
+        print("# \(i) Bingo!!!")
+    }
+    
+    if i % 2 == 0{
+        print("# \(i) par!!!")
+    }
+    
+    if i % 2 != 0{
+        print("# \(i) impar!!!")
+    }
+    
+    if i >= 30 && i <= 40{
+        print("# \(i) Viva Swift!!!")
+    }
+}
+
